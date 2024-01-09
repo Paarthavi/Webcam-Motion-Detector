16eed03a3ffcc0071fb0d8df2eb57db18430e966
Paarthavi <paarthavih2@gmail.com>
2024-01-09 15:23:56 +0530

    Day 37: Send Email when Webcam Detects an Object.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2
 import time
 from emailing import send_email
 import glob
+import os
 
 # 0 for laptop main camera and 1 for usb attached/secondary camera
 video = cv2.VideoCapture(0)
@@ -11,6 +12,11 @@ time.sleep(1)
 first_frame = None
 status_list = []
 count = 1
+
+def clean_folder():
+	images = glob.glob("images/*.png")
+	for image in images:
+		os.remove(image)
 
 while True:
 	status = 0
@@ -63,6 +69,7 @@ while True:
 
 	if status_list[0] == 1 and status_list[1] == 0:
 		send_email(image_with_object)
+		clean_folder()
 
 	cv2.imshow("Video", frame)
 	# waitKey function waits for specified milliseconds for any keyboard event
